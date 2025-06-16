cf8ae70ed9aa6f123ce7c0820365148ef38c4ef0
luccasagazz <daniel.lucca@escola.pr.gov.br>
2025-06-16 19:21:10 -0300

    .

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ geraSenha();
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
-function diminuiTamanho(){
+function diminuiTamanho(){}
 if (tamanhoSenha > 1){
 /*tamanhoSenha = tamanhoSenha-1;*/
 tamanhoSenha--;
 }
 numeroSenha.textContent = tamanhoSenha;
 geraSenha();
-function aumentaTamanho(){
+function aumentaTamanho(){}
 if (tamanhoSenha < 20){
 /*tamanhoSenha = tamanhoSenha+1;*/
 tamanhoSenha++;
